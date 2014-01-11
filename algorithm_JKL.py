535639da3d7c6615ab36d3b49006e6db353ab8c6
wuhongyu1992 <wuhongyu@umich.edu>
2014-01-11 10:58:05 -0500

    r c

--- algorithm_JKL.py
+++ algorithm_JKL.py
@@ -3,6 +3,8 @@ def responseToChallenge(gameData, isPlayingSecond):
     if gameData.selfPoints is 9:
         if len(gameData.selfHand) == 0 and gameData.opponentPoints == 9
             return True
+        else:
+            return False
         if sum(gameData.selfHand)/len(gameData.selfHand) > 7
             return True 
     if gameData.getTrickDiff() >= 3:
