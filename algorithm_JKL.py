9c43d40054c2f88d9d9797b5d0cc3b7795e6236a
Zhang Boren <bobo1993324@gmail.com>
2014-01-10 23:15:56 -0500

    playCard add second

--- algorithm_JKL.py
+++ algorithm_JKL.py
@@ -1,9 +1,13 @@
 def responseToChallenge(gameData, isPlayingSecond):
 	return False;
-def issueChanllenge(gameData, isPlayingSecond, opponentsCard="0"):
+def issueChanllenge(gameData, isPlayingSecond, opponentsCard=0):
 	return False;
 #return the index of card to play
-def playCard(gameData, isPlayingSecond, opponentsCard="0"):
+def playCard(gameData, isPlayingSecond, opponentsCard=0):
 	if isPlayingSecond :
-		print "isPlayingSecond"
-	return 0;
+		for i in gameData.selfHand:
+			if i > opponentsCard:
+				return i
+		# if cannot find a bigger one return the least
+		return gameData.selfHand[0]
+	
