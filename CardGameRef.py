f458b7412e191001100c393de36b4b70fcd1c5f4
ByzanTine <morbyzantine@gmail.com>
2014-01-10 22:11:47 -0500

    Data Updated

--- CardGameRef.py
+++ CardGameRef.py
@@ -8,6 +8,10 @@ class Data:
     opponentHandsum=0
     selfTricks=0
     opponentTricks=0
+    HandDiffCoefficient=0;
+    TrickCoefficient=0;
+
+
     def getRank(self):
         return 1
 
@@ -22,9 +26,15 @@ class Data:
    		return selfTricks
    	def getOpponentTricks(self):
    		return opponentTricks
-   	
+   	def getHandDiffCofficient(self):
+   		return HandDiffCoefficient
+   	def getTrickCoefficient(self):
+   		return TrickCoefficient
     #originate Vals
     def shuffle(self):
+    	if(len(self.deck)!=0):
+			for i in range(0,13):
+				self.deck.pop()
     	for i in range(0,13):
  			#self.decksum=i*8+8+self.decksum
         	self.deck.append(8)
@@ -37,3 +47,5 @@ print x.getRank()
 print len(x.deck)
 print x.decksum
 print x.deck[7]
+x.shuffle()
+print len(x.deck)
