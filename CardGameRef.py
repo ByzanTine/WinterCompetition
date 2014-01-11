10e55444e3544a2159612c4508bbc9f01aa5d6e4
ByzanTine <morbyzantine@gmail.com>
2014-01-10 23:09:55 -0500

    fixed bugs

--- CardGameRef.py
+++ CardGameRef.py
@@ -1,74 +1,45 @@
 # from array import *
 
 class Data:
-    deck=[]
-    decksum=0
-    selfHandsum=0
-    selfHand=[0,0,0,0,0]
-    opponentHandsum=0
-    selfTricks=0
-    opponentTricks=0
-    HandDiffCoefficient=0;
-    TrickCoefficient=0;
+	deck=[]
+	decksum=0
+	selfHandsum=0
+	selfHand=[0,0,0,0,0]
+	opponentHandsum=0
+	selfTricks=0
+	opponentTricks=0
+	HandDiffCoefficient=0
+	TrickCoefficient=0
 
 
-    def getRank(self):
-        return 1
+	def getRank(self):
+		return 1
 
+	def challenge(self):
+		return 1
 
-    def getDecksum(self):
-    	return self.decksum
-    def getSelfHandSum(self):
-    	return self.selfHandsum
-    def getOpponentHandSum(self):
-    	return self.opponentHandsum
-   	def getSelfTricks(self):
-   		return self.selfTricks
-   	def getOpponentTricks(self):
-   		return self.opponentTricks
-   	def getHandDiffCofficient(self):
-   		return self.HandDiffCoefficient
-   	def getTrickCoefficient(self):
-   		return self.TrickCoefficient
-   	def getTrickDiff(self):
-   		return self.selfTricks-self.opponentTricks
-   	def Challenge(self):
-   		Challenge=self.HandDiffCoefficient*(self.selfHandsum - self.opponentHandsum)+self.TrickCoefficient*self.getTrickDiff()
-   		print "Challenge Const"+Challenge
-   		if(Challenge>0):
-   			return True
-   		else:
-   			return False
-
-
-
-
-    #originate Vals
-
-
-
-    def shuffle(self):
-    	self.decksum=0
-    	while(len(self.deck)!=0):
+	def shuffle(self):
+		self.decksum=0
+		while(len(self.deck)!=0):
 			self.deck.pop()
 
 		
-    	for i in range(0,13):
- 			#self.decksum=i*8+8+self.decksum
-        	self.deck.append(8)
-        	self.decksum=i*8+8+self.decksum
+		for i in range(0,13):
+			#self.decksum=i*8+8+self.decksum
+			self.deck.append(8)
+			self.decksum=i*8+8+self.decksum
 
-    def gameStart(self,hand):
-    	self.selfTricks=0
-    	self.opponentTricks=0
-    	self.selfHand=hand
+	def gameStart(self,hand):
+		self.selfTricks=0
+		self.opponentTricks=0
+		self.selfHand=hand
 
-    def gameEnd(self):
-    	self.selfTricks=0
-    	self.opponentTricks=0
-    	while(len(self.selfHand)!=0):
-    		self.selfHand.pop()
-    	self.selfHandsum=0
+	def gameEnd(self):
+		self.selfTricks=0
+		self.opponentTricks=0
+		while(len(self.selfHand)!=0):
+			self.selfHand.pop()
+		self.selfHandsum=0
 
 x=Data()
 x.shuffle()
@@ -76,6 +47,7 @@ print x.getRank()
 print len(x.deck)
 print x.decksum
 print x.deck[7]
+
 x.shuffle()
 print len(x.deck)
 x.gameStart([1,3,4,5,6])
@@ -84,5 +56,6 @@ x.gameEnd()
 print x.selfHand
 x.selfTricks=x.selfTricks+1
 print x.selfTricks
-print x.getDecksum()
-x.Challenge()
+x.challenge()
+
+
