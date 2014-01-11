c1debd2ad6f3d5c7bad3acb48fdfdb93dcbafb61
Zhang Boren <bobo1993324@gmail.com>
2014-01-10 22:15:49 -0500

    add AI parameter changer

--- AI_JKL.py
+++ AI_JKL.py
@@ -1,1 +1,22 @@
-class AI_class
+import random
+class AI:
+	def __init__(self):
+		self.shaking=2;
+		self.shakDecreaseRatio=0.9;
+		self.countPeriod=100;#change parameter after *
+		self.totalCount=0;
+		self.winCount=0;
+	
+	#returns true if there should be a new parameter assignment
+	#false otherwise
+	def registerTournamentResult(self, win):
+		if win:
+			self.winCount+=1;
+		self.totalCount+=1;
+		return self.totalCount >= self.countPeriod
+	def getnewParameters(self, listOfParameters):
+		for x in range(0, len(listOfParameters)):
+			listOfParameters[x]=listOfParameters[x]*(1+self.shaking*(random.random()-0.5));
+		self.shaking*=self.shakDecreaseRatio;
+		return listOfParameters;
+	
