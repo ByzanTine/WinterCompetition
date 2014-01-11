76b51f21f757820eb51c45293db469113ed1d0c3
wuhongyu1992 <wuhongyu@umich.edu>
2014-01-11 11:06:25 -0500

    al

--- algorithm_JKL.py
+++ algorithm_JKL.py
@@ -1,19 +1,19 @@
 import math
 def responseToChallenge(gameData, isPlayingSecond):
-    if gameData.selfPoints is 9:
-        if len(gameData.selfHand) == 0 and gameData.opponentPoints == 9
+    if gameData.selfPoints ==9:
+        if len(gameData.selfHand) == 0 and gameData.opponentPoints == 9:
             return True
         else:
             return False
-        if sum(gameData.selfHand)/len(gameData.selfHand) > 7
+        if sum(gameData.selfHand)/len(gameData.selfHand) > 7:
             return True 
     if gameData.getTrickDiff() >= 3:
         return True
     if gameData.getTrickDiff() <= -3:
         return False
-    if gameData.getTrickDiff() is 2:
+    if gameData.getTrickDiff() ==2:
         return True
-    if gameData.getTrickDiff() is -2:
+    if gameData.getTrickDiff() ==-2:
         return False
     if len(gameData.selfHand)==0:
         if gameData.getTrickDiff()==0 :
@@ -27,15 +27,15 @@ def responseToChallenge(gameData, isPlayingSecond):
     return False;
 def issueChallenge(gameData, isPlayingSecond, opponentsCard=0):
     print "issueChallenge: "+str(gameData.getTrickDiff())
-    if len(gameData.selfHand) is 5 and sum(gameData.selfHand) - gameData.selfHand[0] > 40:
+    if len(gameData.selfHand) ==5 and sum(gameData.selfHand) - gameData.selfHand[0] > 40:
         return True;
-    if len(gameData.selfHand) is 4 and sum(gameData.selfHand) - gameData.selfHand[0] > 30:
+    if len(gameData.selfHand) ==4 and sum(gameData.selfHand) - gameData.selfHand[0] > 30:
         return True;
-    if len(gameData.selfHand) is 3 and sum(gameData.selfHand) - gameData.selfHand[0] > 20 and gameData.getTrickDiff() > 0:
+    if len(gameData.selfHand) ==3 and sum(gameData.selfHand) - gameData.selfHand[0] > 20 and gameData.getTrickDiff() > 0:
         return True;
-    if len(gameData.selfHand) is 1 and sum(gameData.selfHand) = 13 and gameData.getTrickDiff() >= 0:
+    if len(gameData.selfHand) ==1 and sum(gameData.selfHand) == 13 and gameData.getTrickDiff() >= 0:
         return True;
-    if gameData.opponentPoints is 9:
+    if gameData.opponentPoints == 9:
         if isPlayingSecond and gameData.challenge() > gameData.indexsecondBound:
             return True 
         elif gameData.challenge() > gameData.indexfirstBound:
@@ -67,12 +67,12 @@ def playCard(gameData, isPlayingSecond, opponentsCard=0):
 	print "selfHand: "
 	print gameData.selfHand
     sub = gameData.selfHand[0]
-	if isPlayingSecond :
-		for i in gameData.selfHand:
-			if i > opponentsCard:
+    if isPlayingSecond :
+        for i in gameData.selfHand:
+            if i > opponentsCard:
                 if i - opponentsCard <= 5:
                     return i
-                elif sub == opponentsCard
+                elif sub == opponentsCard:
                     return sub
                 else:
                     return gameData.selfHand[0]
