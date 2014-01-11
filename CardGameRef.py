fdb9b84b5913509c925732432cdf7efa8d3b4f15
ByzanTine <morbyzantine@gmail.com>
2014-01-10 22:27:58 -0500

    Data changed

--- CardGameRef.py
+++ CardGameRef.py
@@ -4,7 +4,7 @@ class Data:
     deck=[]
     decksum=0
     selfHandsum=0
-    selfHand=[]
+    selfHand=[0,0,0,0,0]
     opponentHandsum=0
     selfTricks=0
     opponentTricks=0
@@ -30,16 +30,31 @@ class Data:
    		return HandDiffCoefficient
    	def getTrickCoefficient(self):
    		return TrickCoefficient
+   	def getTrickDiff(self):
+   		return selfTricks-opponentTricks
     #originate Vals
     def shuffle(self):
-    	if(len(self.deck)!=0):
-			for i in range(0,13):
-				self.deck.pop()
+    	decksum=0
+    	while(len(self.deck)!=0):
+			self.deck.pop()
+
+		
     	for i in range(0,13):
  			#self.decksum=i*8+8+self.decksum
         	self.deck.append(8)
         	self.decksum=i*8+8+self.decksum
 
+    def gameStart(self,hand):
+    	selfTricks=0
+    	opponentTricks=0
+    	self.selfHand=hand
+
+    def gameEnd(self):
+    	selfTricks=0
+    	opponentTricks=0
+    	while(len(self.selfHand)!=0):
+    		self.selfHand.pop()
+    	selfHandsum=0
 
 x=Data()
 x.shuffle()
@@ -49,3 +64,9 @@ print x.decksum
 print x.deck[7]
 x.shuffle()
 print len(x.deck)
+x.gameStart([1,3,4,5,6])
+print x.selfHand
+x.gameEnd()
+print x.selfHand
+x.selfTricks=x.selfTricks+1
+print x.selfTricks
